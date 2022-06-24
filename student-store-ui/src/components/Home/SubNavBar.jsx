0cc8e0ff8014b0aa5e855480c75b037b44c0a83e
Lauren Lee <86620096+laurenkiyomi@users.noreply.github.com>
2022-06-24 10:54:54 -0700

    Edited SubNavBar styling

--- student-store-ui/src/components/Home/SubNavBar.jsx
+++ student-store-ui/src/components/Home/SubNavBar.jsx
@@ -1,6 +1,8 @@
 import * as React from "react"
 
 export default function SubNavBar( {setSearch, setCategory, search} ) {
+    const [type, setType] = React.useState("all")
+
     const handleOnSearchChange = (event) => {
         setSearch(event.target.value)
     }
@@ -8,24 +10,24 @@ export default function SubNavBar( {setSearch, setCategory, search} ) {
     return (
         <div className="sub-navbar">
             <SearchBar className="search-bar" value={search} handleOnSearchChange={handleOnSearchChange}/>
-            <NavLinks setSearch={setSearch} setCategory={setCategory}/>
+            <NavLinks setSearch={setSearch} setCategory={setCategory} type={type} setType={setType}/>
         </div>
     )
 }
 
-export function NavLinks({ setSearch, setCategory }) {
+export function NavLinks({ setSearch, setCategory, type, setType }) {
     return (
         <ul className="nav-links"> 
-            <NavLink category="all" title="All Categories" setCategory={setCategory} />
-            <NavLink category="clothing" title="Clothing" setCategory={setCategory} />
-            <NavLink category="food" title="Food" setCategory={setCategory} />
-            <NavLink category="accessories" title="Accessories" setCategory={setCategory} />
-            <NavLink category="tech" title="Technology" setCategory={setCategory} />
+            <NavLink category="all" title="All Categories" setCategory={setCategory} type={type} setType={setType} />
+            <NavLink category="clothing" title="Clothing" setCategory={setCategory} type={type} setType={setType} />
+            <NavLink category="food" title="Food" setCategory={setCategory} type={type} setType={setType} />
+            <NavLink category="accessories" title="Accessories" setCategory={setCategory} type={type} setType={setType} />
+            <NavLink category="tech" title="Technology" setCategory={setCategory} type={type} setType={setType} />
         </ul>
     )
 }
 
-export function NavLink({ category, title, setCategory }) {
+export function NavLink({ category, title, setCategory, type, setType }) {
     const changeCategory = () => {
         if (category === "all") {
             setCategory("all")
@@ -37,7 +39,11 @@ export function NavLink({ category, title, setCategory }) {
     }
 
     return (
-        <button className="nav-button" onClick={changeCategory}>{title}</button>
+        <button className={`nav-button ${type == category ? "active" : ""}`} onClick={() => {
+            changeCategory()
+            setType(category)
+        }}
+        >{title}</button>
     )
 }
 
